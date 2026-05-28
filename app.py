f94c3c299762724ed0036a9296b5d10bfd74b50c
Son Hoang <sonhoangn@yahoo.com>
2026-05-28 15:51:57 +0700

    1. Move Admin credential to Streamlit secrets.

--- app.py
+++ app.py
@@ -304,7 +304,6 @@ elif not st.session_state.team_name:
         with st.expander("🔵 Console"):
             admin_pass = st.text_input("Master Password", type="password", key="main_admin_pass")
             if admin_pass == st.secrets["admin_password"]:
-            # if admin_pass == "MBV140Years":
                 st.session_state.admin_override = True
                 st.rerun()
 
